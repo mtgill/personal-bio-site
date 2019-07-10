2d8328ca1dc18b65303f0d06ec7d36b5baae99bf
Matt Gill <mtgill90@outlook.com>
2019-07-10 04:18:18 -0500

    added project links to cards

--- src/javascripts/components/projects/projects.js
+++ src/javascripts/components/projects/projects.js
@@ -8,9 +8,10 @@ const createProjectCards = () => {
       projects.forEach((project) => {
         domString += '<div class="col-lg-4 col-md-6 mb-4 card-group">';
         domString += '<div class="card project-card">';
-        domString += `<div class='title'><h2>${project.title}</h2></div>`;
+        domString += `<div class='title'><h3>${project.title}</h3></div>`;
         domString += `<a href=${project.screenshot} data-lightbox="image-1" data-title="${project.description}" class="d-block mb-1">`;
         domString += `<img src="${project.screenshot}" alt="..." class="img-fluid d-block mx-auto"></a>`;
+        domString += `<h6><a href=${project.url}>View Project</a></h6>`;
         domString += '</div>';
         domString += '</div>';
         domString += '</div>';
