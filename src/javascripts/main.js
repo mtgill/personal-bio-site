44366f4c26c6ba3e095535f4739bd92e2951cbf7
Matt Gill <mtgill90@outlook.com>
2019-07-10 21:31:27 -0500

    hoping this works..

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,67 +1,66 @@
 import projectCards from './components/projects/projects';
 
-const bioDiv = document.getElementById('bioPage');
-const techDiv = document.getElementById('technologiesPage');
-const projectDiv = document.getElementById('projectsPage');
+// const bioDiv = document.getElementById('bioPage');
+// const techDiv = document.getElementById('technologiesPage');
+// const projectDiv = document.getElementById('projectsPage');
 
 
-// printToDom function - takes divId and string
-const printToDom = (divId, textToPrint) => {
-  const selectedDiv = document.getElementById(divId);
-  selectedDiv.innerHTML = textToPrint;
-};
+// // printToDom function - takes divId and string
+// const printToDom = (divId, textToPrint) => {
+//   const selectedDiv = document.getElementById(divId);
+//   selectedDiv.innerHTML = textToPrint;
+// };
 
-const buildBioPage = () => {
-  let domString = '';
-  domString += '<h2>Bio Page</h2>';
-  domString += '<p>Bio Page</p>';
-  printToDom('bioPage', domString);
-};
+// const buildBioPage = () => {
+//   let domString = '';
+//   domString += '<h2>Bio Page</h2>';
+//   domString += '<p>Bio Page</p>';
+//   printToDom('bioPage', domString);
+// };
 
-const buildTechPage = () => {
-  let domString = '';
-  domString += '<h2>Technologies Page</h2>';
-  domString += '<p>Technology page</p>';
-  printToDom('technologiesPage', domString);
-};
+// const buildTechPage = () => {
+//   let domString = '';
+//   domString += '<h2>Technologies Page</h2>';
+//   domString += '<p>Technology page</p>';
+//   printToDom('technologiesPage', domString);
+// };
 
-const navButtonLogic = (e) => {
-  const navTargetId = e.target.id;
+// const navButtonLogic = (e) => {
+//   const navTargetId = e.target.id;
 
-  if (navTargetId === 'bioButton') {
-    if (bioDiv.style.display === 'none') {
-      bioDiv.style.display = 'block';
-    }
-    techDiv.style.display = 'none';
-    projectDiv.style.display = 'none';
-    buildBioPage();
-  } else if (navTargetId === 'techButton') {
-    if (techDiv.style.display === 'none') {
-      techDiv.style.display = 'block';
-    }
-    bioDiv.style.display = 'none';
-    projectDiv.style.display = 'none';
-    buildTechPage();
-  } else if (navTargetId === 'projectButton') {
-    if (projectDiv.style.display === 'none') {
-      projectDiv.style.display = 'flex';
-    }
-    bioDiv.style.display = 'none';
-    techDiv.style.display = 'none';
-    projectCards.createProjectCards();
-  }
-};
+//   if (navTargetId === 'bioButton') {
+//     if (bioDiv.style.display === 'none') {
+//       bioDiv.style.display = 'block';
+//     }
+//     techDiv.style.display = 'none';
+//     projectDiv.style.display = 'none';
+//     buildBioPage();
+//   } else if (navTargetId === 'techButton') {
+//     if (techDiv.style.display === 'none') {
+//       techDiv.style.display = 'block';
+//     }
+//     bioDiv.style.display = 'none';
+//     projectDiv.style.display = 'none';
+//     buildTechPage();
+//   } else if (navTargetId === 'projectButton') {
+//     if (projectDiv.style.display === 'none') {
+//       projectDiv.style.display = 'flex';
+//     }
+//     bioDiv.style.display = 'none';
+//     techDiv.style.display = 'none';
+//     projectCards.createProjectCards();
+//   }
+// };
 
-const addEventListeners = () => {
-  const navButtons = document.getElementsByClassName('nav-link');
-  for (let i = 0; i < navButtons.length; i += 1) {
-    navButtons[i].addEventListener('click', navButtonLogic);
-  }
-};
+// const addEventListeners = () => {
+//   const navButtons = document.getElementsByClassName('nav-link');
+//   for (let i = 0; i < navButtons.length; i += 1) {
+//     navButtons[i].addEventListener('click', navButtonLogic);
+//   }
+// };
 
 const init = () => {
   projectCards.createProjectCards();
-  addEventListeners();
 };
 
 init();
