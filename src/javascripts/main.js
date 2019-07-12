73dfc024e868df8ec00a3e9c6b47155a7dc0e8b8
Matt Gill <mtgill90@outlook.com>
2019-07-11 20:53:31 -0500

    fixed scrolling

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,16 +3,9 @@ import 'jquery';
 
 import '../main.scss';
 
-import '../js/front';
-
 import '../vendor/jquery/jquery.min';
-import '../vendor/onepage-scroll/jquery.onepage-scroll';
-import '../vendor/lightbox2/js/lightbox-plus-jquery';
-import '../vendor/lightbox2/js/lightbox';
+
 import '../vendor/bootstrap/js/bootstrap.bundle.min';
-import '../vendor/onepage-scroll/jquery.onepage-scroll.min';
-import '../vendor/lightbox2/js/lightbox.min';
-import '../vendor/lightbox2/js/lightbox-plus-jquery.min';
 
 import projectCards from './components/projects/projects';
 
